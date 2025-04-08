40cd68f0c177c0944c02aa6d0283642813d5d1f5
DoanDat7804 <167109698+DoanDat7804@users.noreply.github.com>
2025-04-08 07:49:34 +0700

    Hien thi duoc lich hen tren trang cua bac si

--- Frontend/src/apis/doctorApi.js
+++ Frontend/src/apis/doctorApi.js
@@ -1,17 +1,16 @@
 // doctorApi.js
 import api from './index';
 
-const updateAppointment = (id, data, config) =>
-  axios.put(`http://localhost:3000/appointments/${id}/note`, { note: data.note }, config);
-
-export const doctorApi = {
+const doctorApi = {
   getAllDoctors: (config = {}) => api.get('/doctors', config),
   getDoctorById: (id, config = {}) => api.get(`/doctors/${id}`, config),
   createDoctor: (doctorData, config = {}) => api.post('/doctors', doctorData, config),
   updateDoctor: (id, doctorData, config = {}) => api.patch(`/doctors/${id}`, doctorData, config),
   deleteDoctor: (id, config = {}) => api.delete(`/doctors/${id}`, config),
-  getAppointments: (config = {}) => api.get('/appointments/me', config), 
-  // getBusyTimes: (config = {}) => api.get('/doctors/me/busy-times', config),
-  // createBusyTime: (busyTimeData, config = {}) => api.post('/doctors/me/busy-times', busyTimeData, config),
-  // deleteBusyTime: (id, config = {}) => api.delete(`/doctors/me/busy-times/${id}`, config),
+  getAppointments: (config = {}) => api.get('/appointments/me', config),
+  // Thêm hàm updateAppointment vào doctorApi
+  updateAppointment: (id, data, config) =>
+    api.put(`/appointments/${id}/note`, { note: data.note }, config),
 };
+
+export { doctorApi };
